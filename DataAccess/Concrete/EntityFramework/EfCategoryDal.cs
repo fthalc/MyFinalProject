96682a6928502c79bcb017ea4174c803971879f9
Fatih Alıcı <fatih.alici44@gmail.com>
2021-03-03 23:49:11 +0300

    Database

--- DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
